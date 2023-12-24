115107ec237af4b2de9cf408255d5450b6f9f377
MarcoServinDLT <marco.servin@alumnos.udg.mx>
2023-12-23 19:55:18 -0600

    Cambio de objeto por Map como estructura de datos auxiliar.

--- challenges/Day21/challenge.ts
+++ challenges/Day21/challenge.ts
@@ -1,17 +1,18 @@
 export function findBalancedSegment(message: number[]) {
-  let balance = 0;
+  let balance = 0, index = 1;
   let balanced = []
-  const map = {0:0};
-  let index = 1;
+  const map = new Map([[0,0]]);
   for(const bit of message){
     balance += [-1, 1][bit];
-    if(map[balance] != null){
-      balanced = [[map[balance], index-1], balanced]
-      	[+((balanced[1]-balanced[0]) >= 
-        (index-1 - map[balance]))];
+    if(map.has(balance)){
+      balanced = [[map.get(balance), index-1], balanced]
+      	[
+          +((balanced[1]-balanced[0]) >= 
+          (index-1 - map.get(balance)))
+        ];
     }else
-      map[balance] = index
+      map.set(balance, index);
     index++
   }
   return balanced
-}
+} 
